8c62c1c94004f1f087ba13d47c52d62056f203e5
mschilli <mschilli>
2005-07-12 03:42:10 +0000

    added printout

--- eg/blended.pl
+++ eg/blended.pl
@@ -7,10 +7,17 @@ use Net::Amazon;
 use Net::Amazon::Request::Blended;
 
 use Log::Log4perl qw(:easy);
-#Log::Log4perl->easy_init($DEBUG);
+Log::Log4perl->easy_init($DEBUG);
 
 my $ua = Net::Amazon->new(token => 'AMAZON_TOKEN');
 my $response = $ua->search(blended => ($ARGV[0] || "Perl"));
+
+if($response->is_success()) {
+    print $response->total_results(), "\n";
+}
+
+__END__
+
 my @properties = $response->properties();
 
 foreach (@properties) {
