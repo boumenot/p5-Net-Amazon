3eccf60f2f43a37a470471c734341e21ee8d5b0b
B10m <menno@b10m.net>
2012-11-19 19:07:06 +0100

    Added locale support for Spain (es) and Italy (it)

---
+++ lib/Net/Amazon/Validate/ItemSearch/es/Keywords.pm
@@ -0,0 +1,80 @@
+# -*- perl -*-
+# !!! DO NOT EDIT !!!
+# This file was automatically generated.
+package Net::Amazon::Validate::ItemSearch::es::Keywords;
+
+use 5.006;
+use strict;
+use warnings;
+
+sub new {
+    my ($class , %options) = @_;
+    my $self = {
+        '_default' => 'Books',
+        %options,
+    };
+
+    push @{$self->{_options}}, 'Books';
+    push @{$self->{_options}}, 'DVD';
+    push @{$self->{_options}}, 'Electronics';
+    push @{$self->{_options}}, 'ForeignBooks';
+    push @{$self->{_options}}, 'KindleStore';
+    push @{$self->{_options}}, 'Kitchen';
+    push @{$self->{_options}}, 'Music';
+    push @{$self->{_options}}, 'Software';
+    push @{$self->{_options}}, 'Toys';
+    push @{$self->{_options}}, 'VideoGames';
+    push @{$self->{_options}}, 'Watches';
+
+    bless $self, $class;
+}
+
+sub user_or_default {
+    my ($self, $user) = @_;
+    if (defined $user && length($user) > 0) {    
+        return $self->find_match($user);
+    } 
+    return $self->default();
+}
+
+sub default {
+    my ($self) = @_;
+    return $self->{_default};
+}
+
+sub find_match {
+    my ($self, $value) = @_;
+    for (@{$self->{_options}}) {
+        return $_ if lc($_) eq lc($value);
+    }
+    die "$value is not a valid value for es::Keywords!\n";
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Net::Amazon::Validate::ItemSearch::es::Keywords - valid search indicies
+for the es locale and the Keywords SearchIndex.
+
+=head1 DESCRIPTION
+
+The default value is Books, unless mode is specified.
+
+The list of available values are:
+
+    Books
+    DVD
+    Electronics
+    ForeignBooks
+    KindleStore
+    Kitchen
+    Music
+    Software
+    Toys
+    VideoGames
+    Watches
+
+=cut
